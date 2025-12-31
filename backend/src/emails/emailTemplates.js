dda319308f1e046e07d6726a92a2abda2cdf07a1
Riya Garg <riyagarg@Riyas-MacBook-Air.local>
2025-12-31 16:40:40 +0530

    messenger to chatify

--- backend/src/emails/emailTemplates.js
+++ backend/src/emails/emailTemplates.js
@@ -5,7 +5,7 @@ export function createWelcomeEmailTemplate(name, clientURL) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Welcome to Messenger</title>
+  <title>Welcome to Chatify</title>
 </head>
 
 <body style="
